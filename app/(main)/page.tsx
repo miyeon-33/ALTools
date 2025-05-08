71cb89486ebccf4a5a58a2b7a2d150cbac750096
miyeon-33 <elav0303@gmail.com>
2025-05-08 14:24:40 +0900

    feat: 메인 업데이트

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,11 +1,15 @@
+import License from '@/app/components/home/License';
 import MainSlider from '@/app/components/home/MainSlider';
 import ProductList from '@/app/components/home/ProductList';
+import Subscribe from '@/app/components/home/Subscribe';
 
 export default function Home() {
   return (
     <main className="pt-[80px]">
       <MainSlider />
       <ProductList />
+      <Subscribe />
+      <License />
       {/* <Link href="/products">products</Link>
       <Link href="/articles/breaking-news-123">read in korean</Link>
       <Link href="/articles/breaking-news-123?lang=en">read in english</Link>
